91ca4b1aa13adf36ea1fd0f7a80567b95f26751f
nBrocki <brocki@posteo.de>
2023-06-12 18:23:12 +0200

    Zwischenstand sichern

--- source/main.js
+++ source/main.js
@@ -1,8 +1,8 @@
 // Carousel reference: https:// .metafizzy.co/
-const carouselConfig =  {
+const carouselConfig = {
     draggable: true,
     freeScroll: false,
-    contain: true,
+    contain: false,
     // disable previous & next buttons and dots
     prevNextButtons: true,
     pageDots: false,
@@ -10,7 +10,7 @@ const carouselConfig =  {
     setGallerySize: false
 };
 
-const detailCarouselConfig =  {
+const detailCarouselConfig = {
     draggable: true,
     freeScroll: false,
     contain: true,
@@ -21,69 +21,75 @@ const detailCarouselConfig =  {
     setGallerySize: false
 };
 
-var flkty = new Flickity('.main-carousel', carouselConfig);
-var detailFlkty = new Flickity('.detail-carousel', detailCarouselConfig);
+var flkty;
+var detailFlkty;
 
-setTimeout(function(){flkty.resize()}, 200);
+window.addEventListener("load", function (event) {
+
+    console.log("loaded");
+    flkty = new Flickity('.main-carousel', carouselConfig);
+    // detailFlkty = new Flickity('.detail-carousel', detailCarouselConfig);
+    setTimeout(function () {
+       flkty.resize();
+        // Open overlay
+        flkty.on('staticClick', function (event, pointer, cellElement, cellIndex) {
+
+            let urlList = cellElement.querySelector("img").getAttribute("data-content-url");
+            const isMobile = window.innerWidth <= 600;
+            getDetailImgs(urlList, isMobile).forEach(imgElement => {
+                detailFlkty.append(imgElement);
+            });
+
+            detailFlkty.select(isMobile ? 1 : 0, false, true);
+            contentOverlay.style.display = "block";
+            setTimeout(() => { detailFlkty.resize() }, 100);
+        });
+    }, 1500);
+
+})
+
 
 // Overlay
 
 const contentOverlay = document.getElementById("detail-overlay");
 const detailCarousel = contentOverlay.querySelector(".detail-carousel");
 
-function closeOverlay(){
+function closeOverlay() {
     contentOverlay.style.display = "none";
-   
+
     console.log(detailCarousel.childNodes);
     detailFlkty.remove(detailCarousel.querySelectorAll(".carousel-cell"));
 }
 
-// Open overlay
-flkty.on('staticClick', function( event, pointer, cellElement, cellIndex ) {
-    console.log(cellElement)
-    // every item can be opened
-    //if(!cellElement.classList.contains("is-selected")) return;
-    
-    let urlList = cellElement.querySelector("img").getAttribute("data-content-url");
-    const isMobile = window.innerWidth <= 600;
-    getDetailImgs(urlList, isMobile).forEach(imgElement => {
-        detailFlkty.append(imgElement);
-    });
-
-    detailFlkty.select(isMobile ? 1 : 0, false, true);
-    contentOverlay.style.display = "block";
-    setTimeout(() => {detailFlkty.resize()}, 100);
-});
-
-function getDetailImgs(urlString, isMobile){
+function getDetailImgs(urlString, isMobile) {
     dataImg = [];
     urlString.split(",").forEach(photoUrl => {
-        
+
         dataImg.push(getDetailImgElement(photoUrl));
         // If its mobile, create a second copy of the elements
-        if(isMobile)    
+        if (isMobile)
             dataImg.push(getDetailImgElement(photoUrl, "push-left", true));
-    }); 
+    });
     return dataImg;
 }
 
-function getDetailImgElement(url, imgClassName, hasContainer){
+function getDetailImgElement(url, imgClassName, hasContainer) {
     const newDiv = document.createElement("div");
     newDiv.className = "carousel-cell";
-    
+
     const img = document.createElement("img");
-    if(imgClassName)
+    if (imgClassName)
         img.className = imgClassName;
     img.setAttribute("src", url);
-    
-    if(hasContainer){
+
+    if (hasContainer) {
         const containerDiv = document.createElement("div");
         containerDiv.className = "container";
         containerDiv.appendChild(img);
         newDiv.appendChild(containerDiv);
-    }else{
+    } else {
         newDiv.appendChild(img);
     }
-    
+
     return newDiv;
 }
